669f7844c03c83d50b8f2ba6e90d31f599e128f0
keeprok <140785214+keeprok@users.noreply.github.com>
2024-03-15 22:56:25 +0900

    button 완성

---
+++ begginer-2/src/libs/desigin-token/border.js
@@ -0,0 +1,12 @@
+export const border = {
+    size: {
+        sm: "0.1rem",
+        md: "0.2rem",
+        bg: "0.3rem",
+    },
+    radius: {
+        square: "0rem",
+        oval: "1.2rem",
+        circle: "3rem",
+    },
+};
